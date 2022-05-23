9af1be0dd9df81ffe45ddd48149e38cdbf6a896b
colombolucas <colombo.lucasit@gmail.com>
2022-05-23 14:42:22 -0300

    my first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Versão Inicial"),
+    );
+  }
+}
