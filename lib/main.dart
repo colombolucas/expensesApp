e41f4102e0e5daa49b39a89202a4b632c4f43133
colombolucas <colombo.lucasit@gmail.com>
2022-05-23 15:51:15 -0300

    Adicionado models e implementado na main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(ExpensesApp());
@@ -5,15 +6,65 @@ void main() => runApp(ExpensesApp());
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      home: MyHomePage(),
+    );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Cinema com a Gata',
+      value: 60.0,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Versão Inicial"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Despesas Pessoais"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Text("Gráfico"),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                  child: Row(
+                children: [
+                  Container(
+                    child: Text(tr.value.toString()),
+                  ),
+                  Column(
+                    children: [
+                      Text(tr.title),
+                      Text(tr.date.toString()),
+                    ],
+                  )
+                ],
+              ));
+            }).toList(),
+          )
+        ],
+      ),
     );
   }
 }
